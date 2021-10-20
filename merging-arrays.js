fe943c53cb13865f604d241e64b990252bf94cda
selvamani <selvamani05@ymail.com>
2021-10-20 23:15:44 +0530

    javascript-merging-arrays-all-methods-js

--- merging-arrays.js
+++ merging-arrays.js
@@ -101,3 +101,23 @@ console.log('**** 5. Using reduce() Method ****');
     console.log(`input array ${JSON.stringify(arr2)}`); // [4,5,6]
     console.log(`merged using reduce() method: ${JSON.stringify(merged)}`); // [1,2,3,4,5,6]
 }
+
+console.log('22-array-filter-sort-map');
+
+// Filter the array of students with Higest Ranking, Sort on Ranking, finally Show the Names 
+
+const studentsArray = [
+    { name: 'Suraj', year: 2019, ranking: 4 },
+    { name: 'Amit', year: 2019, ranking: 5 },
+    { name: 'Akash', year: 2018, ranking: 4 },
+    { name: 'Dinanath', year: 2019, ranking: 7 },
+    { name: 'Sagar', year: 2017, ranking: 3 },
+]
+
+console.log('Higest Rank Holders:',
+    studentsArray
+        .filter(student => student.year === 2019 && student.ranking >= 5)
+        .sort((n1, n2) => n1.ranking - n2.ranking)
+        .reverse()
+        .map(student => student.name)
+);
